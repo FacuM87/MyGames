effa50825ef0ce8ab6287e378916b6144e1e5e58
FacuM87 <facundo.mingorance@hotmail.com>
2025-05-19 11:03:52 -0300

    Carousel fix

--- frontend/src/components/Carousel.jsx
+++ frontend/src/components/Carousel.jsx
@@ -47,11 +47,7 @@ const Carousel = () => {
                       }`
                     }>
                       
-            <img
-              src={slide.image}
-              alt={slide.title}
-              className="carousel-image"
-            />
+            <img src={slide.image} alt={slide.title} className="carousel-image"/>
             <div className="absolute bottom-0 left-0 right-0 p-4 bg-gradient-to-t from-black/70 to-transparent">
               <h3 className="text-2xl font-bold text-white">{slide.title}</h3>
             </div>
@@ -61,9 +57,7 @@ const Carousel = () => {
 
       <div className="absolute bottom-4 left-1/2 transform -translate-x-1/2 flex space-x-2">
         {slides.map((_, index) => (
-          <button
-            key={index}
-            onClick={() => setCurrentSlide(index)}
+          <button key={index} onClick={() => setCurrentSlide(index)}
             className={`w-3 h-3 rounded-full transition-colors duration-200 ${
               index === currentSlide ? 'bg-white' : 'bg-white/50'
             }`}
